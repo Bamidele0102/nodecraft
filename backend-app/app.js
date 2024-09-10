e3d8adb7f84fbd1ba376eeaa1ff721a0a445d99c
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-09-10 07:14:45 +0100

    Installed env and connected MongoDB to the App

--- backend-app/app.js
+++ backend-app/app.js
@@ -6,12 +6,15 @@ const itemsRoute = require('./routes/items');
 const app = express();
 app.use(express.json());
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
 
 // Routes
 app.use('/api/items', itemsRoute);
 
-// Health check
-// app.get('/health', (req, res) => res.status(200).send('API is healthy'));
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
