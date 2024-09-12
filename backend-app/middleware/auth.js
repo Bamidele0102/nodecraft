fc533baea784c2b5b19adb766361165aa046d47a
Victor Amajuoyi <victorgavon@gmail.com>
2024-09-12 22:58:22 +0100

    Implemented Middleware with JWT Authentication

---
+++ backend-app/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+
+module.exports = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ message: 'Access denied' });
+
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(400).json({ message: 'Invalid token' });
+    }
+};
