79ee54b7085b135699e6251b8dd0188c3a931ebf
Victor Amajuoyi <victorgavon@gmail.com>
2024-09-12 23:01:46 +0100

    Creating the User model

---
+++ backend-app/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
