5d2e6a7c3dfbbab40dc9bcdf805071e2ef3772e3
Victor Amajuoyi <victorgavon@gmail.com>
2024-09-12 23:02:48 +0100

    Updating the User model

--- backend-app/models/User.js
+++ backend-app/models/User.js
@@ -13,3 +13,17 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
 });
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+
+UserSchema.methods.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+
+module.exports = mongoose.model('User', UserSchema);
