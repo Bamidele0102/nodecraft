16a248dc424115b17e37cbec8e9a4047c425da3f
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-09-13 00:46:03 +0100

    Added logging to the Mongo connection

--- backend-app/app.js
+++ backend-app/app.js
@@ -43,8 +43,8 @@ app.use(limiter);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.log(err));
+    .then(() => logger.info('Connected to MongoDB'))
+    .catch(err => logger.error('MongoDB connection error:', err));
 
 // Swagger documentation route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
